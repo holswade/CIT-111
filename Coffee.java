33a6a2d02e2736cd271e4325e52bc2eb556c6b8e
holswade <43549646+holswade@users.noreply.github.com>
2018-11-07 11:51:37 -0500

    Add files via upload
    cofffeeeeeee

--- Coffee.java
+++ Coffee.java
@@ -5,6 +5,8 @@
  */
 package classstructure;
 
+
+import java.util.Scanner;
 /**
  *
  * @author lauren.holswade319
@@ -13,13 +15,32 @@ public class Coffee {
    public static int totalPumpkinSpiceLattes = 0;
     public static int totalPeppermintMocha = 0;
     public static double totalSales = 0.0;
+    public static int orderpumpkinspice;
+    public static int orderpeppermintmocha;
     public static final double PRICE_PUMPKIN_SPICE = 4.50;
     public static final double PRICE_PEPPERMINT_MOCHA = 5.00;
     
+    
+    public static Scanner userPerson = new Scanner (System.in);
+    int order = o; 
+    
     public static void main(String[] args) {
         // calling method while passing in 12 as an input
+        if(order == 1){
+            compScore = compScore + 10;
+            System.out.println(" Do you want a pumpkin spice latte? ");
+        } else { 
+            System.out.println (" Ugh then would like a peppermint mocha ? ");
+        }// end if/else blocks
+        
+        int order = 1; 
+        displayOrderTotals();    
+
         buyPumpkinSpiceLattes(12);
-        displayOrderTotals();
+        buyPeppermintMocha(20);
+        
+       System.out.println("Do you seasonal coffee? ( 0 = no, 1 = yes)");
+
         
     }// close main method
     
@@ -29,22 +50,39 @@ public class Coffee {
 
     public static void buyPumpkinSpiceLattes (int numPump){
       totalPumpkinSpiceLattes = totalPumpkinSpiceLattes + numPump;
-        System.out.println("Number of Pumpkin Spice lattes ordered: ");  
+        System.out.println("Number of Pumpkin Spice lattes ordered: "+totalPumpkinSpiceLattes);  
         totalSales = totalSales + numPump * PRICE_PUMPKIN_SPICE;
-        
+        System.out.println("Making Pumpkin Spice lattes: " + totalPumpkinSpiceLattes);
+        System.out.println("Total Pumpkin Spice Latte ordered: " + totalPumpkinSpiceLattes);
+        System.out.println("Total price " + totalSales);
         
     
     } // end buyPumpkinSpiceLattes
     
-   public static void printPumpkin(){
-       System.out.println("Total Pumpkin Spice lattes: " + totalPumpkinSpiceLattes);
-   }
-    public static void displayOrderTotals(){
-        System.out.println("**********ORDER TOTALS*************");
-        System.out.println("Total Pumpkin Spice Latte ordered: " + totalPumpkinSpiceLattes);
+    public static void buyPeppermintMocha (int numPump){
+      totalPeppermintMocha = totalPeppermintMocha + numPump;
+        System.out.println("Number of Peppermint Mochas ordered: "+totalPeppermintMocha);  
+        totalSales = totalSales + numPump * PRICE_PEPPERMINT_MOCHA;
+        System.out.println("Making Peppermint Mochas : " + totalPeppermintMocha);
+         System.out.println("Total Peppermint Mocha ordered "+ totalPeppermintMocha);
         System.out.println("Total price " + totalSales);
+    } // end buyPeppermintMocha
         
-        System.out.println("Total Peppermint Mocha ordered");
+   public static void printPumpkin(){
+       System.out.println("Making Pumpkin Spice lattes: " + totalPumpkinSpiceLattes);
+       totalSales = totalSales + 4.5; 
+   }
+    
+   public static void printPeppermint(){
+       System.out.println("Making Peppermint Mochas : " + totalPeppermintMocha);
+       totalSales = totalSales + 5.00; 
+   }
+   
+   
+   
+   public static void displayOrderTotals(){
+        System.out.println("**********ORDER TOTALS*************");
+       
         
         
     }
